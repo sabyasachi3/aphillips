73fb05fac4ff16b0db9419c25bd1c65edbb21b09
sharedocs1@gmail.com <sharedocs1@gmail.com@a05c6118-58dd-11de-8d7d-01d876f1962d>
2009-11-10 10:14:27 +0000

    Added a varargs version of colEq.

--- commons-test-support/trunk/src/main/java/com/qrmedia/commons/test/easymock/CollectionEqualsMatcher.java
+++ commons-test-support/trunk/src/main/java/com/qrmedia/commons/test/easymock/CollectionEqualsMatcher.java
@@ -5,6 +5,7 @@ package com.qrmedia.commons.test.easymock;
 
 import static org.easymock.EasyMock.reportMatcher;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -43,13 +44,13 @@ public class CollectionEqualsMatcher implements IArgumentMatcher {
     /* (non-Javadoc)
      * @see org.easymock.IArgumentMatcher#matches(java.lang.Object)
      */
-    public boolean matches(Object collection) {
+    public boolean matches(Object object) {
         /*
          * The expected collection is not null, so a null input should return false. Note that
          * instanceof will fail for null input.
          */
-        return ((collection instanceof Collection<?>) 
-                && CollectionUtils.isEqualCollection(expectedCollection, (Collection<?>) collection));
+        return ((object instanceof Collection<?>) 
+                && CollectionUtils.isEqualCollection(expectedCollection, (Collection<?>) object));
     }
     
     /**
@@ -58,10 +59,23 @@ public class CollectionEqualsMatcher implements IArgumentMatcher {
      * 
      * @param expectedCollection    the (non-<code>null</code>) collection expected
      * @return  the given expected collection
+     * @see #colEq(Object...)
      */
     public static <T> Collection<T> colEq(Collection<T> expectedCollection) {
         reportMatcher(new CollectionEqualsMatcher(expectedCollection));
         return expectedCollection;
     }
 
+    /**
+     * Factory method to register a matcher which will compare the vararg collection passed
+     * by the code under test against the expected collection.
+     * 
+     * @param expectedCollection    the collection expected
+     * @return  the given expected collection
+     * @see #colEq(Collection)
+     */
+    public static <T> Collection<T> colEq(T... expectedCollection) {
+        return colEq(Arrays.asList(expectedCollection));
+    }
+    
 }
