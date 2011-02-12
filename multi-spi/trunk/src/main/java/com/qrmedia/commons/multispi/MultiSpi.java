ccfbffefb67420e61ff8e0f325915ff1d86df4b0
sharedocs1 <sharedocs1@gmail.com>
2011-02-12 15:29:49 +0000

    Now falling back to the system, then the bootstrap classloader if the thread context loader is null.

--- multi-spi/trunk/src/main/java/com/qrmedia/commons/multispi/MultiSpi.java
+++ multi-spi/trunk/src/main/java/com/qrmedia/commons/multispi/MultiSpi.java
@@ -21,19 +21,23 @@
 package com.qrmedia.commons.multispi;
 
 import static com.google.common.base.Predicates.not;
-import static com.google.common.collect.Iterables.any;
-import static com.google.common.collect.Iterables.concat;
-import static com.google.common.collect.Iterables.filter;
-import static com.google.common.collect.Iterables.transform;
+import static com.google.common.base.Predicates.notNull;
+import static com.google.common.collect.Iterables.*;
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Sets.newHashSet;
+import static com.google.common.collect.Sets.newHashSetWithExpectedSize;
 import static java.lang.String.format;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ServiceLoader;
 import java.util.Set;
 
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Inject;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.qrmedia.commons.multispi.config.MultiSpiBuilder;
@@ -43,14 +47,18 @@ import com.qrmedia.commons.multispi.provider.ServiceImplementationProvider;
 /**
  * One-stop shop for users of MultiSpi. Instances should be created via dependency
  * injection or using the {@link MultiSpiBuilder}.
- * 
+ *
  * @author aphillips
+ * @see ServiceLoader
  * @since 4 Dec 2010
  *
  */
 @ThreadSafe
 public final class MultiSpi {
     private final Set<ServiceImplementationProvider> providers = newHashSet();
+    // ready for @Inject
+    private final InstanceFactory instanceFactory = new PublicNoargConstructorInstanceFactory();
+    private final ClassLoaderLocator loaderLocator = new ClassLoaderLocator();
     
     @Inject
     public MultiSpi(@Nonnull Set<ServiceImplementationProvider> providers) {
@@ -76,7 +84,13 @@ public final class MultiSpi {
     }
     
     /**
-     * Shorthand for {@link #findImplementations(Class, ClassLoader) findImplementations(serviceClass, MultiSpi.class.getClassLoader())}.
+     * Shorthand for {@link #findImplementations(Class, ClassLoader) findImplementations(serviceClass, defaultClassLoader)}
+     * where {@code defaultClassLoader} is
+     * <ul>
+     * <li>{@link Thread#getContextClassLoader() Thread.currentThread().getContextClassLoader()} if non-<code>null</code>
+     * <li>{@link ClassLoader#getSystemClassLoader()} if non-<code>null</code>
+     * <li>the bootstrap classloader otherwise
+     * </ul>
      * 
      * @param <S> the type of the service
      * @param serviceClass the class of the service
@@ -88,7 +102,12 @@ public final class MultiSpi {
      */
     public @Nonnull <S> Set<Class<? extends S>> findImplementations(
             @Nonnull Class<S> serviceClass) throws ClassCastException, ClassNotFoundException {
-        return findImplementations(serviceClass, MultiSpi.class.getClassLoader());
+        return findImplementations(serviceClass, findDefaultLoader());
+    }
+    
+    private ClassLoader findDefaultLoader() {
+        return find(newArrayList(Thread.currentThread().getContextClassLoader(),
+                loaderLocator.getSystemClassLoader(), loaderLocator.getBootstrapClassLoader()), notNull());
     }
     
     /**
@@ -105,8 +124,8 @@ public final class MultiSpi {
      * 
      * @param <S> the type of the service
      * @param serviceClass the class of the service
-     * @param implementationLoader the class loader to be used to attempt to load the classed
-     * @return a set of classes of implementations of the service
+     * @param implementationLoader the class loader to be used to attempt to load the classes
+     * @return a set of instances of implementations of the service
      * @throws ClassCastException if any of the classes are not actually implementations
      *                            of the requested service
      * @throws ClassNotFoundException if any of the classes cannot be loaded                           
@@ -133,6 +152,7 @@ public final class MultiSpi {
         return new AssignableFromPredicate(superclass);
     }
 
+    // TODO: move to Predicates
     private static class AssignableFromPredicate implements Predicate<Class<?>> {
         private final Class<?> superclass;
         
@@ -144,7 +164,131 @@ public final class MultiSpi {
             return superclass.isAssignableFrom(input);
         }
     }
+    
+    /**
+     * Shorthand for {@link #loadImplementations(Class, ClassLoader) loadImplementations(serviceClass, defaultClassLoader)}.
+     * where {@code defaultClassLoader} is
+     * <ul>
+     * <li>{@link Thread#getContextClassLoader() Thread.currentThread().getContextClassLoader()} if non-<code>null</code>
+     * <li>{@link ClassLoader#getSystemClassLoader()} if non-<code>null</code>
+     * <li>the bootstrap classloader otherwise
+     * </ul>
+     * 
+     * @param <S> the type of the service
+     * @param serviceClass the class of the service
+     * @return a set of instances of implementations of the service
+     * @throws ClassNotFoundException if any of the classes cannot be loaded 
+     * @throws InstantiationException if any of the classes are not actually implementations
+     *          of the requested service, or cannot be instantiated using a public no-argument 
+     *          constructor                   
+     * @see #loadImplementations(Class, ClassLoader)
+     */
+    public @Nonnull <S> Set<S> loadImplementations(@Nonnull Class<S> serviceClass) 
+            throws ClassNotFoundException, InstantiationException {
+        return loadImplementations(serviceClass, findDefaultLoader());
+    }
 
+    /**
+     * Finds the classes implementing the requested service, as determined by the 
+     * available providers, and loads an instance of each implementation.
+     * <p>
+     * This method will check whether the classes found do, in fact, implement the
+     * requested service, and an {@code InstantiationException} will be thrown if any
+     * of the classes does not.
+     * <p>
+     * Service classes are expected to provide a public, no-argument constructor, which
+     * will be invoked to create instances of the implementation classes found. An
+     * {@code InstantiationException} is thrown if the class cannot be instantiated.
+     * <p>
+     * If more lenient behaviour is desired (e.g. skipping any classes that cannot be
+     * found, or do not actually implement the interface), use 
+     * {@link #findImplementationNames(Class)} instead.
+     * 
+     * @param <S> the type of the service
+     * @param serviceClass the class of the service
+     * @param implementationLoader the class loader to be used to attempt to load the classes
+     * @return a set of instances of implementations of the service
+     * @throws ClassNotFoundException if any of the classes cannot be loaded 
+     * @throws InstantiationException if any of the classes are not actually implementations
+     *          of the requested service, or cannot be instantiated using a public no-argument 
+     *          constructor                   
+     * @see #loadImplementations(Class)
+     */
+    public @Nonnull <S> Set<S> loadImplementations(@Nonnull Class<S> serviceClass, 
+            @Nonnull final ClassLoader implementationLoader) throws ClassNotFoundException, 
+                InstantiationException {
+        Set<Class<? extends S>> implementationClasses;
+        try {
+            implementationClasses = findImplementations(serviceClass, implementationLoader);
+        } catch (ClassCastException exception) {
+            throw new InstantiationException(format("Unable to load implementation classes for service class '%s' due to: %s",
+                    serviceClass, exception.getMessage()));
+        }
+        
+        Set<S> implementations = newHashSetWithExpectedSize(implementationClasses.size());
+        // can't use Iterables.transform because we want to throw the exception
+        for (Class<? extends S> implementationClass : implementationClasses) {
+            implementations.add(instanceFactory.newInstance(implementationClass));
+        }
+        return implementations;
+    }
+    
+    // ready to be factored out into an external interface with pluggable implementations
+    private static interface InstanceFactory {
+        @Nonnull <T> T newInstance(@Nonnull Class<T> clazz) throws InstantiationException;
+    }
+    
+    private static class PublicNoargConstructorInstanceFactory implements InstanceFactory {
+        public <T> T newInstance(Class<T> clazz) throws InstantiationException {
+            Constructor<T> publicNoargConstructor;
+            try {
+                publicNoargConstructor = clazz.getConstructor();
+            } catch (NoSuchMethodException exception) {
+                throw new InstantiationException(format("Unable to find public no-argument constructor for class '%s' due to: %s",
+                        clazz, exception.getMessage()));
+            }
+            
+            try {
+                return publicNoargConstructor.newInstance();
+            } catch (InvocationTargetException exception) {
+                throw new InstantiationException(format("Unable to create new instance of class '%s' due to exception in constructor '%s': %s: %s",
+                        clazz, publicNoargConstructor, exception.getCause(), exception.getCause().getMessage()));
+            } catch (IllegalAccessException exception) {
+                // should not happen - the constructor takes no args and we're calling it with no args
+                throw new AssertionError(format("Caught IllegalAccessException '%s' calling public constructor '%s' for class '%s'?!?", 
+                        exception.getMessage(), publicNoargConstructor, clazz));
+            } catch (IllegalArgumentException exception) {
+                // should not happen - the constructor takes no args and we're calling it with no args
+                throw new AssertionError(format("Caught IllegalArgumentException '%s' calling no-argument constructor '%s' for class '%s' with no arguments?!?", 
+                        exception.getMessage(), publicNoargConstructor, clazz));
+            }
+        }
+    }
+    
+    @VisibleForTesting
+    @ThreadSafe
+    static class ClassLoaderLocator {
+        private static final ClassLoader BOOTSTRAP_LOADER = new BootstrapClassLoader();
+        
+        @Nonnull ClassLoader getSystemClassLoader() {
+            return ClassLoader.getSystemClassLoader();
+        }
+        
+        @Nonnull ClassLoader getBootstrapClassLoader() {
+            return BOOTSTRAP_LOADER;
+        }
+        
+        private static class BootstrapClassLoader extends ClassLoader {
+            private BootstrapClassLoader() {
+                /*
+                 * The default classloader implementation will use the bootstrap loader
+                 * if it finds a null parent.
+                 */
+                super(null);
+            }
+        }
+    }
+    
     /* (non-Javadoc)
      * @see java.lang.Object#hashCode()
      */
--- multi-spi/trunk/src/test/java/com/qrmedia/commons/multispi/MultiSpiTest.java
+++ multi-spi/trunk/src/test/java/com/qrmedia/commons/multispi/MultiSpiTest.java
@@ -23,6 +23,7 @@ package com.qrmedia.commons.multispi;
 import static com.google.common.collect.Sets.newHashSet;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.classextension.EasyMock.createMock;
+import static org.easymock.classextension.EasyMock.createNiceMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
@@ -34,7 +35,9 @@ import uk.gov.mi6.agent.JackGiddings;
 import uk.gov.mi6.agent.JamesBond;
 import uk.gov.mi6.agent.StuartThomas;
 
+import com.qrmedia.commons.multispi.MultiSpi.ClassLoaderLocator;
 import com.qrmedia.commons.multispi.provider.ServiceImplementationProvider;
+import com.qrmedia.commons.reflect.ReflectionUtils;
 
 /**
  * Unit tests for the {@link MultiSpi}.
@@ -78,6 +81,86 @@ public class MultiSpiTest {
         multiSpi = new MultiSpi(newHashSet(provider));
         assertEquals(newHashSet(JamesBond.class), 
                 multiSpi.findImplementations(Agent.class, implementationLoader));
+        
+        verify(implementationLoader);
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void loadsImplementationsFromContextClassloaderByDefault() throws ClassNotFoundException {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(JamesBond.class.getName()));
+        ClassLoader contextLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(contextLoader.loadClass(JamesBond.class.getName())).andReturn((Class) JamesBond.class);
+        replay(provider, contextLoader);
+        
+        final ClassLoader originalContextLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(contextLoader);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        try {
+            assertEquals(newHashSet(JamesBond.class), multiSpi.findImplementations(Agent.class));
+            verify(contextLoader);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextLoader);
+        }
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void loadsImplementationsFromSystemClassloaderForNullContextLoader() throws ClassNotFoundException, IllegalAccessException {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(JamesBond.class.getName()));
+        ClassLoader systemLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(systemLoader.loadClass(JamesBond.class.getName())).andReturn((Class) JamesBond.class);
+        ClassLoaderLocator loaderLocator = createNiceMock(ClassLoaderLocator.class);
+        expect(loaderLocator.getSystemClassLoader()).andReturn(systemLoader);
+        replay(provider, systemLoader, loaderLocator);
+        
+        final ClassLoader originalContextLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(null);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        ReflectionUtils.setValue(multiSpi, "loaderLocator", loaderLocator);
+        try {
+            assertEquals(newHashSet(JamesBond.class), multiSpi.findImplementations(Agent.class));
+            verify(systemLoader);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextLoader);
+        }
+    }
+    
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void loadsImplementationsFromBootstrapClassloaderForNullSystemLoader() throws ClassNotFoundException, IllegalAccessException {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(JamesBond.class.getName()));
+        ClassLoader bootstrapLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(bootstrapLoader.loadClass(JamesBond.class.getName())).andReturn((Class) JamesBond.class);
+        
+        // will return null for other loaders        
+        ClassLoaderLocator loaderLocator = createNiceMock(ClassLoaderLocator.class);
+        expect(loaderLocator.getBootstrapClassLoader()).andReturn(bootstrapLoader);
+        replay(provider, bootstrapLoader, loaderLocator);
+        
+        final ClassLoader originalContextLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(null);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        ReflectionUtils.setValue(multiSpi, "loaderLocator", loaderLocator);
+        try {
+            assertEquals(newHashSet(JamesBond.class), multiSpi.findImplementations(Agent.class));
+            verify(bootstrapLoader);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextLoader);
+        }
     }
     
     @Test(expected = ClassCastException.class)
@@ -89,5 +172,155 @@ public class MultiSpiTest {
         
         multiSpi = new MultiSpi(newHashSet(provider));
         multiSpi.findImplementations(Agent.class);
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void loadsImplementationsForInstantiationFromProvidedClassloader() throws Exception {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(JamesBond.class.getName()));
+        ClassLoader implementationLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(implementationLoader.loadClass(JamesBond.class.getName()))
+        .andReturn((Class) JamesBond.class);
+        replay(provider, implementationLoader);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        assertEquals(newHashSet(new JamesBond()), 
+                multiSpi.loadImplementations(Agent.class, implementationLoader));
+        
+        verify(implementationLoader);
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void loadsImplementationsForInstantiationFromContextClassloaderByDefault() throws Exception {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(JamesBond.class.getName()));
+        ClassLoader contextLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(contextLoader.loadClass(JamesBond.class.getName())).andReturn((Class) JamesBond.class);
+        replay(provider, contextLoader);
+        
+        final ClassLoader originalContextLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(contextLoader);
+
+        multiSpi = new MultiSpi(newHashSet(provider));
+        try {
+            assertEquals(newHashSet(new JamesBond()), multiSpi.loadImplementations(Agent.class));
+            verify(contextLoader);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextLoader);
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void loadsImplementationsForInstantiationFromSystemClassloaderForNullContextLoader() throws Exception {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(JamesBond.class.getName()));
+        ClassLoader systemLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(systemLoader.loadClass(JamesBond.class.getName())).andReturn((Class) JamesBond.class);
+        ClassLoaderLocator loaderLocator = createNiceMock(ClassLoaderLocator.class);
+        expect(loaderLocator.getSystemClassLoader()).andReturn(systemLoader);
+        replay(provider, systemLoader, loaderLocator);
+        
+        final ClassLoader originalContextLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(null);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        ReflectionUtils.setValue(multiSpi, "loaderLocator", loaderLocator);
+        try {
+            assertEquals(newHashSet(new JamesBond()), multiSpi.loadImplementations(Agent.class));
+            verify(systemLoader);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextLoader);
+        }
+    }
+    
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void loadsImplementationsForInstantiationFromBootstrapClassloaderForNullSystemLoader() throws Exception {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(JamesBond.class.getName()));
+        ClassLoader bootstrapLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(bootstrapLoader.loadClass(JamesBond.class.getName())).andReturn((Class) JamesBond.class);
+
+        // will return null for other loaders
+        ClassLoaderLocator loaderLocator = createNiceMock(ClassLoaderLocator.class);
+        expect(loaderLocator.getBootstrapClassLoader()).andReturn(bootstrapLoader);
+        replay(provider, bootstrapLoader, loaderLocator);
+        
+        final ClassLoader originalContextLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(null);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        ReflectionUtils.setValue(multiSpi, "loaderLocator", loaderLocator);
+        try {
+            assertEquals(newHashSet(new JamesBond()), multiSpi.loadImplementations(Agent.class));
+            verify(bootstrapLoader);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextLoader);
+        }
+    }    
+    
+    @Test(expected = InstantiationException.class)
+    public void convertsClassCastExceptionDuringImplementationLoading() throws Exception {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(String.class.getName()));
+        replay(provider);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        multiSpi.loadImplementations(Agent.class);        
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test(expected = InstantiationException.class)
+    public void throwsInstantiationExceptionOnMissingPublicNoargConstructor() throws Exception {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(AgentWithoutNoargConstructor.class.getName()));
+        ClassLoader implementationLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(implementationLoader.loadClass(AgentWithoutNoargConstructor.class.getName()))
+        .andReturn((Class) AgentWithoutNoargConstructor.class);
+        replay(provider, implementationLoader);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        multiSpi.loadImplementations(Agent.class, implementationLoader);
+    }
+
+    public static class AgentWithoutNoargConstructor implements Agent {
+        public AgentWithoutNoargConstructor(Object arg) {}
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test(expected = InstantiationException.class)
+    public void convertsInvocationTargetExceptionsDuringInstantiation() throws Exception {
+        ServiceImplementationProvider provider = createMock(ServiceImplementationProvider.class);
+        expect(provider.findServiceImplementations(Agent.class))
+        .andReturn(newHashSet(AgentWithExceptionThrowingConstructor.class.getName()));
+        ClassLoader implementationLoader = createMock(ClassLoader.class);
+        // hack generics
+        expect(implementationLoader.loadClass(AgentWithExceptionThrowingConstructor.class.getName()))
+        .andReturn((Class) AgentWithExceptionThrowingConstructor.class);
+        replay(provider, implementationLoader);
+        
+        multiSpi = new MultiSpi(newHashSet(provider));
+        multiSpi.loadImplementations(Agent.class, implementationLoader);               
+    }
+    
+    public static class AgentWithExceptionThrowingConstructor implements Agent {
+        public AgentWithExceptionThrowingConstructor() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
